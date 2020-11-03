55ddd115715ed677dc5d155b9fb808ace13a4886
Garv nanwani <garvnanwani16@gmail.com>
2020-11-03 13:32:54 +0530

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { applyMiddleware, compose, createStore } from "redux"
+import thunk from "redux-thumk"
 import App from "./App"
+import reducers from "./reducers"
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(<App />, document.getElementById("root"))
