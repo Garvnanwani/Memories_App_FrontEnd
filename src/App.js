3c9bdcfbaf123a44cea094973c079f7c811664bb
Garv nanwani <garvnanwani16@gmail.com>
2020-11-03 13:10:21 +0530

    basic component setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { AppBar, Container, Grid, Grow, Typography } from "@material-ui/core"
 import React from "react"
+import Form from "./components/Form/Form"
+import Posts from "./components/Posts/Posts"
+import memories from "./images/memories.png"
 
 const App = () => {
-  return <div>Hii</div>
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
 }
 
 export default App
